fa85e3ebef4b2a0b4e4fca1626849b729cead590
Zeeshan <zeeshanakram2919@gmail.com>
2026-08-14 14:07:59 +0500

    Fix Railway HTTPS proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(
-            at: '*'
-        );
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
